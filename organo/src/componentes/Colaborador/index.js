82896f496ec430d96a63e95bb774cc390548c1f9
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-02-20 06:47:34 -0300

    4º Passo |  Mapeando os colaboradores

--- organo/src/componentes/Colaborador/index.js
+++ organo/src/componentes/Colaborador/index.js
@@ -1,15 +1,15 @@
 import './Colaborador.css'
 
-const Colaborador = () => {
+const Colaborador = ({nome,imagem,cargo}) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src='https://github.com/MeirellesBrito.png' alt='Meirelles'/>
+                <img src={imagem} alt={nome}/>
             </div>
             
             <div className='rodape'>
-                <h4>Meirelles</h4>
-                <h5>Dev</h5>
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
             </div>
 
         </div>
