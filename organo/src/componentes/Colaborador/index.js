ff9ca19f774c79eabb7b083bdb5335c42aeaf42b
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-02-20 06:23:49 -0300

    4º Passo |  Criando Card de colaborador

---
+++ organo/src/componentes/Colaborador/index.js
@@ -0,0 +1,19 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/MeirellesBrito.png' alt='Meirelles'/>
+            </div>
+            
+            <div className='rodape'>
+                <h4>Meirelles</h4>
+                <h5>Dev</h5>
+            </div>
+
+        </div>
+    )
+}
+ 
+export default Colaborador
